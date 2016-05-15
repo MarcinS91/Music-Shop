42af7c40531a1b1280119c1aa890a802a5dbd14b
MarcinS91 <szewc.marcin91@gmail.com>
2016-05-15 15:17:06 +0200

    add product class

--- musicstore/src/main/java/com/packt/musicstore/domain/Product.java
+++ musicstore/src/main/java/com/packt/musicstore/domain/Product.java
@@ -1,8 +1,59 @@
-package com.packt.music.store.domain;
+package com.packt.musicstore.domain;
 
 public class Product {
 	private String productName;
 	private String productCategory;
 	private String productDescription;
-	
+	private double productPrice;
+	private String productStatus;
+	private int unitInStock;
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductCategory() {
+		return productCategory;
+	}
+
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
+	}
+
+	public String getProductDescription() {
+		return productDescription;
+	}
+
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+
+	public double getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public String getProductStatus() {
+		return productStatus;
+	}
+
+	public void setProductStatus(String productStatus) {
+		this.productStatus = productStatus;
+	}
+
+	public int getUnitInStock() {
+		return unitInStock;
+	}
+
+	public void setUnitInStock(int unitInStock) {
+		this.unitInStock = unitInStock;
+	}
+
 }
