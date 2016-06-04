60d7e1fc885c4b1dbeba8cde7c3bdc934aa79b5b
MarcinS91 <szewc.marcin91@gmail.com>
2016-06-04 12:58:58 +0200

    rename pacage

--- musicstore/src/main/java/com/packt/musicstore/model/Product.java
+++ musicstore/src/main/java/com/packt/musicstore/model/Product.java
@@ -1,12 +1,53 @@
-package com.packt.musicstore.domain;
+package com.packt.musicstore.model;
 
-public class Product {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = -3532377236419382983L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int productId;
+
+	@NotEmpty(message = "Nazwa produktu nie mo¿e pozostaæ pusta!")
 	private String productName;
+
 	private String productCategory;
 	private String productDescription;
+
+	@Min(value = 0, message = "Cena produktu powinna byæ wiêksza od zera")
 	private double productPrice;
+	private String productCondition;
 	private String productStatus;
+
+	@Min(value = 0, message = "Ilosc sztuk nie moze byc mniejsza od zera!")
 	private int unitInStock;
+	private String productManufacturer;
+
+	@Transient
+	private MultipartFile productImage;
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
 
 	public String getProductName() {
 		return productName;
@@ -55,5 +96,31 @@ public class Product {
 	public void setUnitInStock(int unitInStock) {
 		this.unitInStock = unitInStock;
 	}
+
+	public MultipartFile getProductImage() {
+		return productImage;
+	}
+
+	public void setProductImage(MultipartFile productImage) {
+		this.productImage = productImage;
+	}
+
+	public String getProductCondition() {
+		return productCondition;
+	}
+
+	public void setProductCondition(String productCondition) {
+		this.productCondition = productCondition;
+	}
+
+	public String getProductManufacturer() {
+		return productManufacturer;
+	}
+
+	public void setProductManufacturer(String productManufacturer) {
+		this.productManufacturer = productManufacturer;
+	}
+	
+	
 
 }
