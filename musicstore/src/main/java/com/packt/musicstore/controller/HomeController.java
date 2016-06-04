ebe4e34e1ea4505cafa324cda9a9f85bccd86cf5
MarcinS91 <szewc.marcin91@gmail.com>
2016-06-04 12:53:48 +0200

    modify

--- musicstore/src/main/java/com/packt/musicstore/controller/HomeController.java
+++ musicstore/src/main/java/com/packt/musicstore/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.packt.musicstore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -11,7 +12,22 @@ public class HomeController {
 	public String welcome(Model model) {
 		model.addAttribute("greeting", "Witaj w sklepie internetowym!");
 		model.addAttribute("tagline", "Wyj¹tkowym i jedynym sklepie internetowym");
-		
+
 		return "welcome";
+	}
+
+	@RequestMapping("/login")
+	public String login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout, Model model) {
+
+		if (error != null) {
+			model.addAttribute(error, "Niepoprawna nazwa uzytkownika i haslo!");
+		}
+
+		if (logout != null) {
+			model.addAttribute("msg", "Wylogowanie zakonczone z sukcesem!");
+		}
+
+		return "login";
 	}
 }
