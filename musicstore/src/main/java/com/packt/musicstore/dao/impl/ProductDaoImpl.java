f3e81b8503f53b5724c7c60226da6427dada67fd
MarcinS91 <szewc.marcin91@gmail.com>
2016-06-04 12:59:38 +0200

    create ProductDaoImpl

---
+++ musicstore/src/main/java/com/packt/musicstore/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,60 @@
+package com.packt.musicstore.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.packt.musicstore.dao.ProductDao;
+import com.packt.musicstore.model.Product;
+
+@Repository
+@Transactional
+public class ProductDaoImpl implements ProductDao {
+
+	@Autowired(required = true)
+	private SessionFactory sessionFactory;
+
+	public void addProduct(Product product) {
+		Session session = sessionFactory.getCurrentSession();
+		session.saveOrUpdate(product);
+		session.flush();
+
+	}
+
+	public void deleteProduct(Product product) {
+		Session session = sessionFactory.getCurrentSession();
+		session.delete(product);
+		session.flush();
+
+	}
+
+	public void editProduct(Product product) {
+		Session session = sessionFactory.getCurrentSession();
+		session.saveOrUpdate(product);
+		session.flush();
+
+	}
+
+	public Product getProductById(int id) {
+		Session session = sessionFactory.getCurrentSession();
+		Product product = (Product) session.get(Product.class, id);
+		session.flush();
+
+		return product;
+	}
+
+	public List<Product> getProductList() {
+		Session session = sessionFactory.getCurrentSession();
+		Query query = session.createQuery("from Product");
+		List<Product> productList = query.list();
+		session.flush();
+		
+		return productList;
+	}
+
+}
