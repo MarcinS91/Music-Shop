920ed7e9043a8179584e46906d6e4e9ef3f93379
MarcinS91 <szewc.marcin91@gmail.com>
2016-07-31 16:51:02 +0200

    add admincustomer

---
+++ musicstore/src/main/java/com/packt/musicstore/admin/AdminCustomer.java
@@ -0,0 +1,34 @@
+package com.packt.musicstore.admin;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.packt.musicstore.model.Customer;
+import com.packt.musicstore.service.CustomerService;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminCustomer {
+
+		
+	@Autowired
+	private CustomerService customerService;
+
+	@RequestMapping("/customer/deleteCustomer/{id}")
+	public String deleteCustomer(@PathVariable int id, Model model, HttpServletRequest request) {
+		
+		Customer customer = customerService.getCustomerById(id);
+		customerService.deleteCustomer(customer);
+
+		return "redirect:/admin/customer";
+	}
+}
