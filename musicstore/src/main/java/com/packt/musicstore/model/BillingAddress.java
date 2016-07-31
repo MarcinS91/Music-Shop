11aceec006b1e513dcad1ceb298838ee63b6acb0
MarcinS91 <szewc.marcin91@gmail.com>
2016-07-31 16:57:09 +0200

    add BillingAddress.java

---
+++ musicstore/src/main/java/com/packt/musicstore/model/BillingAddress.java
@@ -0,0 +1,89 @@
+package com.packt.musicstore.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class BillingAddress implements Serializable {
+
+	private static final long serialVersionUID = -6091579459463730482L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int billingAddressId;
+
+	private String streetName;
+	private String streetNumber;
+	private String city;
+	private String country;
+	private String zipCode;
+
+	@OneToOne(cascade = CascadeType.REMOVE)
+	private Customer customer;
+	
+	@OneToOne(cascade = CascadeType.REMOVE, mappedBy = "billingAddress")
+	private CustomerOrder customerOrder;
+
+	public int getBillingAddressId() {
+		return billingAddressId;
+	}
+
+	public void setBillingAddressId(int billingAddressId) {
+		this.billingAddressId = billingAddressId;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+
+	public void setStreetNumber(String streetNumber) {
+		this.streetNumber = streetNumber;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+}
