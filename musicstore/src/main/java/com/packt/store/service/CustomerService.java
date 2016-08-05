e0bb294f74f65cfcbdb1c88b16f72352978d7cca
MarcinS91 <szewc.marcin91@gmail.com>
2016-08-05 18:35:18 +0200

    create CustomerService

---
+++ musicstore/src/main/java/com/packt/store/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.packt.store.service;
+
+import java.util.List;
+
+import com.packt.store.model.Customer;
+
+public interface CustomerService {
+
+	void addCustomer(Customer customer);
+	
+	void deleteCustomer(Customer customer);
+	
+	Customer getCustomerById(int customerId);
+	
+	List<Customer> getAllCustomers();
+	
+	Customer getCustomerByUsername(String username);
+}
