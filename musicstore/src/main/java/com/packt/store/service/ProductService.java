7410ee999957f0cf6e9f5e46ec1a0abd2947fb18
MarcinS91 <szewc.marcin91@gmail.com>
2016-08-05 18:36:20 +0200

    rename

--- musicstore/src/main/java/com/packt/store/service/ProductService.java
+++ musicstore/src/main/java/com/packt/store/service/ProductService.java
@@ -1,8 +1,8 @@
-package com.packt.musicstore.service;
+package com.packt.store.service;
 
 import java.util.List;
 
-import com.packt.musicstore.model.Product;
+import com.packt.store.model.Product;
 
 public interface ProductService {
 	
